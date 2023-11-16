8947b5d5edfd3e35b6a7e5a2d2b415a9a98d2d88
Caszarjos <zaratecastro2003@hotmail.com>
2023-11-16 14:55:06 -0500

    feat(Cost): Added cost controller

--- AyniWebBackend/Ayni/Controllers/CostsController.cs
+++ AyniWebBackend/Ayni/Controllers/CostsController.cs
@@ -1,3 +1,4 @@
+using System.Net.Mime;
 using AutoMapper;
 using AyniWebBackend.Ayni.Domain.Models;
 using AyniWebBackend.Ayni.Domain.Services;
@@ -5,35 +6,40 @@ using AyniWebBackend.Ayni.Resources;
 using AyniWebBackend.Shared.Extensions;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace AyniWebBackend.Ayni.Controllers;
 
 [EnableCors("ReglasCors")]
 [ApiController]
+[Produces(MediaTypeNames.Application.Json)]
+[SwaggerTag("Costs Management Endpoints")]
 [Route("/api/v1/[controller]")]
-public class CostController : ControllerBase
+public class CostsController : ControllerBase
 {
     private readonly ICostService _costService;
     private readonly IMapper _mapper;
 
-    public CostController(ICostService costService, IMapper mapper)
+    public CostsController(ICostService costService, IMapper mapper)
     {
         _costService = costService;
         _mapper = mapper;
     }
-
-    //methods
+    
     [HttpGet]
+    [ProducesResponseType(typeof(IEnumerable<CostResource>), 200)]
     public async Task<IEnumerable<CostResource>> GetAllAsync()
     {
         var costs = await _costService.ListAsync();
-        var resources = _mapper.Map<IEnumerable<Cost>, 
-            IEnumerable<CostResource>>(costs);
+        var resources = _mapper.Map<IEnumerable<Cost>, IEnumerable<CostResource>>(costs);
         return resources;
 
     }
     
     [HttpPost]
+    [ProducesResponseType(typeof(CostResource), 201)]
+    [ProducesResponseType(typeof(List<string>), 400)]
+    [ProducesResponseType(500)]
     public async Task<IActionResult> PostAsync([FromBody] 
         SaveCostResource resource)
     {
@@ -44,9 +50,8 @@ public class CostController : ControllerBase
         var result = await _costService.SaveAsync(cost);
         if (!result.Success)
             return BadRequest(result.Message);
-        var costResource = _mapper.Map<Cost, 
-            CostResource>(result.Resource);
-        return Ok(costResource);
+        var costResource = _mapper.Map<Cost, CostResource>(result.Resource);
+        return Created(nameof(PostAsync), costResource);
     }
     
     [HttpPut("{id}")]
